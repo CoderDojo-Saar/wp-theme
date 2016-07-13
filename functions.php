95fe4fbe68af13a511f5f7205d0d7c7d90874124
Andreas Sander <andreas.sander@hotmail.de>
2016-07-13 22:16:46 +0200

    Remove style enqueuing in functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,1 @@
-<?php
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css'
-    );
-}
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
+<?php ?>
